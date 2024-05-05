39fbbcb50253533b57e248f569ca78a0c76e021f
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-05 14:44:06 +0600

    chore: Update Navbar component layout and styling

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,15 +1,17 @@
-import { ThemeToggler } from "@/components/theme/theme-toggler"
-import { icons } from "@/components/icons"
-import { siteConfig } from "@/config/site.config"
+import { ThemeToggler } from "@/components/theme/theme-toggler";
+import { icons } from "@/components/icons";
+import { siteConfig } from "@/config/site.config";
 
 export default function Navbar() {
   return (
-    <nav className="w-full py-2 px-3 flex justify-between items-center border-b">
-      <div className="flex items-center gap-2">
-        <icons.logo width={24} height={24} className="m-1" />
-        <h1 className="text-lg font-bold">{siteConfig.name}</h1>
+    <nav className="w-full py-2 px-3 border-b">
+      <div className="w-full mx-auto max-w-3xl flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <icons.logo width={24} height={24} className="m-1" />
+          <h1 className="text-lg font-bold">{siteConfig.name}</h1>
+        </div>
+        <ThemeToggler />
       </div>
-      <ThemeToggler />
     </nav>
-  )
+  );
 }
