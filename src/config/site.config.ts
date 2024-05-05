4a19bd932363f7de7d67cfbf5ce74147a70eb946
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-05 14:01:40 +0600

    site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,24 @@
+import type { SiteConfig } from '@/types'
+
+export const siteConfig: SiteConfig = {
+  title: "Vite + React + ShadCN",
+  name: "Vite + React + ShadCN",
+  description: "This is a starter template for Vite initialized react, typescript, tailwindcss, shadcn. It supports dark mode, and is ready to deploy to Vercel.",
+  keywords: [
+    "vite",
+    "react",
+    "typescript",
+    "tailwindcss",
+    "shadcn",
+    "vercel",
+    "starter"
+  ],
+  author: "rds_agi",
+  siteUrl: "https://vite-react-shadcn.vercel.app",
+  ogImage: "https://vite-react-shadcn.vercel.app/og.png",
+  links: {
+    github: "https://github.com/rudrodip/vite-react-shadcn",
+    twitter: "https://twitter.com/rds_agi",
+    deploy: "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Frudrodip%2Fvite-react-shadcn"
+  }
+}
