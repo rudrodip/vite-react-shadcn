fbf1d96ea3fcfbda7ce2ce6e8d29c11e798e103d
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 08:29:58 +0600

    refactored silly spelling mistake

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@/components/theme/theme-provider"
 
-export default function Proivders({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       {children}
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@/styles/globals.css'
-import Proivders from '@/components/providers.tsx'
+import Providers from '@/components/providers.tsx'
 import Navbar from "@/components/navbar";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Proivders>
+    <Providers>
       <main className='min-h-screen'>
         <Navbar />
         <App />
       </main>
-    </Proivders>
+    </Providers>
   </React.StrictMode>,
 )
