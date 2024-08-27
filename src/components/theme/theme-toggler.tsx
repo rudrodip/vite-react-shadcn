570c04845ba7f67884dd4bf62c6fe7aa3ea0e4ab
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-27 09:53:39 +0600

    updated

--- src/components/theme/theme-toggler.tsx
+++ src/components/theme/theme-toggler.tsx
@@ -1,37 +1,66 @@
-import { Moon, Sun } from "lucide-react"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme/theme-provider"
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "./theme-provider";
+import { Button } from "@/components/ui/button";
+import { useState, useEffect } from "react";
 
 export function ThemeToggler() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
+  const [systemTheme, setSystemTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setSystemTheme(mediaQuery.matches ? "dark" : "light");
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setSystemTheme(e.matches ? "dark" : "light");
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  const SWITCH = () => {
+    switch (theme) {
+      case "light":
+        setTheme("dark");
+        break;
+      case "dark":
+        setTheme("light");
+        break;
+      case "system":
+        setTheme(systemTheme === "light" ? "dark" : "light");
+        break;
+      default:
+        break;
+    }
+  };
+
+  const TOGGLE_THEME = () => {
+    //@ts-ignore
+    if (!document.startViewTransition) SWITCH();
+
+    //@ts-ignore
+    document.startViewTransition(SWITCH);
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+    <Button
+      onClick={TOGGLE_THEME}
+      variant="ghost"
+      size="sm"
+      className="size-8 p-0"
+    >
+      <SunIcon
+        size={16}
+        className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      />
+      <MoonIcon
+        size={16}
+        className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
 }
